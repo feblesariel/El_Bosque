417b394c19ad828775ec7bbabc4b4c201833ae92
feblesariel <feblesariel@gmail.com>
2024-02-19 19:50:35 +0000

    initial commit

---
+++ src/app.js
@@ -0,0 +1,50 @@
+// ************ Require's ************
+
+const express = require('express');
+const session = require("express-session");
+const path = require('path');
+const methodOverride = require('method-override'); // Para poder usar los métodos PUT y DELETE.
+
+// ************ express() - (don't touch) ************
+
+const app = express();
+
+// ************ Middlewares - (don't touch) ************
+
+const publicPath = path.resolve(__dirname, '../public');
+app.use(session({
+    secret: "Secreto",
+    resave: false,
+    saveUninitialized: false,
+}));
+app.use(express.static(publicPath));  // Indica donde estan los archivos estáticos.
+app.use(express.urlencoded({ extended: false }));  // Captura la informacion enviada por POST.
+app.use(express.json()); // Solicitudes entrantes con formato JSON.
+app.use(methodOverride('_method'));  // Para poder pisar el method="POST" en el formulario por PUT y DELETE.
+
+// ************ Template Engine - (don't touch) ************
+
+app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, '/views')); // Define la ubicación de la carpeta de las vistas.
+
+// ************ Route System require and use() ************
+
+// const homeRouter = require("./routes/home");
+// const productsRouter = require("./routes/products");
+// const searchRouter = require("./routes/search");
+// const usersRouter = require("./routes/users");
+// const cartRouter = require("./routes/cart");
+// const settingsRouter = require("./routes/settings");
+
+// app.use('/', homeRouter);
+// app.use('/products/', productsRouter);
+// app.use('/search/', searchRouter);
+// app.use('/users/', usersRouter);
+// app.use('/cart/', cartRouter);
+// app.use('/settings/', settingsRouter);
+
+// ************ Run server ************
+
+app.listen(3000, () => {
+    console.log('Server running in 3000 port');
+});
