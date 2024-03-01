8512e80cc444245995e4f16b73e4e412bdee059f
feblesariel <feblesariel@gmail.com>
2024-03-01 07:42:17 +0000

    detail

--- db/el_bosque.sql
+++ db/el_bosque.sql
@@ -121,46 +121,40 @@ CREATE TABLE subscribers (
 
 -- Poblar tablas
 
-INSERT INTO categories (name) VALUES ('Categoria 1'), ('Categoria 2'), ('Categoria 3'), ('Categoria 4');
+INSERT INTO categories (name) VALUES ('Categoria 1'), ('Categoria 2'), ('Categoria 3');
 
 INSERT INTO products (category_id, name, price, description) VALUES
-    (1, 'Torta 1', 499.99, 'Descripción del producto torta 1'),
-    (1, 'Torta 2', 299.99, 'Descripción del producto torta 2'),
-    (1, 'Torta 3', 899.99, 'Descripción del producto torta 3'),
-    (1, 'Torta 4', 79.99, 'Descripción del producto torta 4'),
-    (2, 'Budin 1', 19.99, 'Descripción del producto budin 1'),
-    (2, 'Budin 2', 39.99, 'Descripción del producto budin 2'),
-    (2, 'Budin 3', 49.99, 'Descripción del producto budin 3'),
-    (2, 'Budin 4', 99.99, 'Descripción del producto budin 4'),
-    (3, 'Desayuno 1', 599.99, 'Descripción del producto desayuno 1'),
-    (3, 'Desayuno 2', 299.99, 'Descripción del producto desayuno 2'),
-    (3, 'Desayuno 3', 79.99, 'Descripción del producto desayuno 3'),
-    (3, 'Desayuno 4', 39.99, 'Descripción del producto desayuno 4'),
-    (4, 'Salado 1', 29.99, 'Descripción del producto salado 1'),
-    (4, 'Salado 2', 19.99, 'Descripción del producto salado 2'),
-    (4, 'Salado 3', 24.99, 'Descripción del producto salado 3'),
-    (4, 'Salado 4', 39.99, 'Descripción del producto salado 4');
+    (1, 'Producto 1', 499.99, 'Clásica chocotorta con ganache de chocolate como topping y deco con picos de dulce de leche, chocolatines y chips de chocolate.'),
+    (2, 'Producto 2', 299.99, 'Clásica chocotorta con ganache de chocolate como topping y deco con picos de dulce de leche, chocolatines y chips de chocolate.'),
+    (3, 'Producto 3', 899.99, 'Clásica chocotorta con ganache de chocolate como topping y deco con picos de dulce de leche, chocolatines y chips de chocolate.'),
+    (1, 'Producto 4', 79.99, 'Clásica chocotorta con ganache de chocolate como topping y deco con picos de dulce de leche, chocolatines y chips de chocolate.'),
+    (2, 'Producto 5', 19.99, 'Clásica chocotorta con ganache de chocolate como topping y deco con picos de dulce de leche, chocolatines y chips de chocolate.');
 
 INSERT INTO product_options (id_product, option_type, option_value, description) VALUES
-    (1, 'Tamaño', 'Chico', 'Tamaño chico de 4 porciones'),
-    (2, 'Tamaño', 'Chico', 'Tamaño chico de 4 porciones'),
-    (3, 'Tamaño', 'Chico', 'Tamaño chico de 4 porciones'),
-    (4, 'Tamaño', 'Chico', 'Tamaño chico de 4 porciones');
+    (1, 'Tamaño', 'Chica', 'Rinde para 8 porciones.'),
+    (1, 'Relleno', 'Dulce de leche', 'Relleno de dulce de leche Vacalin.'),
+    (2, 'Tamaño', 'Chica', 'Rinde para 8 porciones.'),
+    (2, 'Relleno', 'Dulce de leche', 'Relleno de dulce de leche Vacalin.'),
+    (3, 'Tamaño', 'Chica', 'Rinde para 8 porciones.'),
+    (3, 'Relleno', 'Dulce de leche', 'Relleno de dulce de leche Vacalin.'),
+    (4, 'Tamaño', 'Chica', 'Rinde para 8 porciones.'),
+    (4, 'Relleno', 'Dulce de leche', 'Relleno de dulce de leche Vacalin.'),
+    (5, 'Tamaño', 'Chica', 'Rinde para 8 porciones.'),
+    (5, 'Relleno', 'Dulce de leche', 'Relleno de dulce de leche Vacalin.');
 
 INSERT INTO product_images (product_id, url) VALUES
-    (1, 'product-1.jpg'),
-    (2, 'product-2.jpg'),
-    (3, 'product-3.jpg'),
-    (4, 'product-4.jpg'),
-    (5, 'product-1.jpg'),
-    (6, 'product-2.jpg'),
-    (7, 'product-3.jpg'),
-    (8, 'product-4.jpg'),
-    (9, 'product-1.jpg'),
-    (10, 'product-2.jpg'),
-    (11, 'product-3.jpg'),
-    (12, 'product-4.jpg'),
-    (13, 'product-1.jpg'),
-    (14, 'product-2.jpg'),
-    (15, 'product-3.jpg'),
-    (16, 'product-4.jpg');
+    (1, 'product-big-1.jpg'),
+    (1, 'product-big-2.jpg'),
+    (1, 'product-big-3.jpg'),
+    (2, 'product-big-1.jpg'),
+    (2, 'product-big-2.jpg'),
+    (2, 'product-big-3.jpg'),
+    (3, 'product-big-1.jpg'),
+    (3, 'product-big-2.jpg'),
+    (3, 'product-big-3jpg'),
+    (4, 'product-big-1.jpg'),
+    (4, 'product-big-2.jpg'),
+    (4, 'product-big-3.jpg'),
+    (5, 'product-big-1.jpg'),
+    (5, 'product-big-2.jpg'),
+    (5, 'product-big-3.jpg');
