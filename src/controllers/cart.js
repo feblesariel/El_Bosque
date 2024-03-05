20951911f8792ffb7ab1f820328715ef6983158d
feblesariel <feblesariel@gmail.com>
2024-03-05 19:39:36 +0000

    cart

--- src/controllers/cart.js
+++ src/controllers/cart.js
@@ -64,7 +64,7 @@ const cartController = {
 
             if (!cart[cartProductId]) { // Si el producto aún no está en el carrito, agrégalo.
                 cart[cartProductId] = {
-                    c: productId,
+                    product_id: productId,
                     category: product.Category.name,
                     name: product.name,
                     selectedOptions: selectedOptions,
