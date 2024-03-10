d0af220b2dd2d397ff358e458518028ee144291c
feblesariel <feblesariel@gmail.com>
2024-03-10 10:24:53 +0000

    checkout

--- public/js/search.js
+++ public/js/search.js
@@ -1,9 +1,9 @@
-$(document).ready(function() {
-    $('#searchForm').submit(function(event) {
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('searchForm').addEventListener('submit', function(event) {
         event.preventDefault(); // Evita que se envíe el formulario normalmente
         
         // Obtén el valor del campo de búsqueda
-        const searchTerm = $('#headerSearch').val();
+        const searchTerm = document.getElementById('headerSearch').value;
         
         // Construye la URL de destino con el parámetro de búsqueda
         const destinationUrl = '/products/shop?search=' + encodeURIComponent(searchTerm);
