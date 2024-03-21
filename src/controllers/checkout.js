568b04b6e61c63ec065ee1b566d364d3b9bc66ea
feblesariel <feblesariel@gmail.com>
2024-03-21 23:13:51 +0000

    checkout

--- src/controllers/checkout.js
+++ src/controllers/checkout.js
@@ -111,7 +111,7 @@ const checkoutController = {
 
                     }            
 
-                    cart.deliveryMethod = {type: orderType, id: delivery.id};
+                    cart.deliveryMethod = orderType;
 
                     cart.total = parseFloat(newTotal).toFixed(2);
 
@@ -151,7 +151,7 @@ const checkoutController = {
 
                     }
 
-                    cart.deliveryMethod = {type: orderType, id: delivery.id};
+                    cart.deliveryMethod = orderType;
 
                     cart.total = (newTotal).toFixed(2);
 
@@ -196,7 +196,7 @@ const checkoutController = {
 
                     }
 
-                    cart.deliveryMethod = {type: orderType, id: delivery.id};
+                    cart.deliveryMethod = orderType;
 
                     cart.total = (newTotal).toFixed(2);
 
@@ -315,7 +315,7 @@ const checkoutController = {
         // Obtener fecha en milisegundos para generar el codigo unico.
         const milliseconds = Date.now();
         // Creo variable para almacenar datos para enviar al front.
-        let summary = req.cookies.summary || { method: payMethod, order: {}, items: cart.item, client: { name: name, tel: tel, email: email, note: note, zipcode: postcode, city: city, address: address}};
+        let summary = req.cookies.summary || { orderType: orderType, payMethod: payMethod, order: {}, items: cart.item, client: { name: name, tel: tel, email: email, note: note, postcode: postcode, city: city, address: address}};
 
         // Crea discount en summary si cart.discount existe.
         if (cart.discount) {
@@ -323,7 +323,7 @@ const checkoutController = {
         }
 
         // Si el metodo de envio es delivery ingreso los datos del envio en la cookie summary.
-        if (cart.deliveryMethod && cart.deliveryMethod.type === "delivery") {
+        if (cart.deliveryMethod && cart.deliveryMethod === "delivery") {
 
             obtenerDelivery();
 
@@ -460,7 +460,7 @@ const checkoutController = {
                             address: address,
                             city: city,
                             postal_code: postcode,
-                            delivery_id: cart.deliveryMethod.id
+                            delivery_id: summary.delivery.id
                         };
                         return Order_detail_delivery.create(deliveryDetails);
                     }
